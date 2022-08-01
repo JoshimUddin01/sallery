261dd5f8cfd47939d35da2ca4a0451b5f762a6a4
Joshim Uddin <JoshimUddin01>
2022-08-01 21:30:56 +0600

    forth commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let add_btn = document.querySelector(".add");
 
 add_btn.addEventListener("click", ()=>{
    let employe_name = document.querySelector("#user_name").value;
-   let salery = document.querySelector("#user_sallery").value;
+   let salary = document.querySelector("#user_sallery").value;
    
    let  regex = new RegExp(/[^0-9]/);
-   if(regex.test(salery) === true) {
+   if(regex.test(salary) === true) {
      let modal_click = document.querySelector(".modal_btn");
      modal_click.click();
    }
